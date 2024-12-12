61f884fda7cc80df0fd4a9c22ee0c49935565d7c
nkhaccuong <cuong19012002@gmail.com>
2024-12-12 23:26:58 +0700

    update vnpay url

--- src/services/vnpayService.js
+++ src/services/vnpayService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Retrieve Backend URL from .env file
 // const VNPAY_URL = "http://localhost:5000/api/vnpay";
-const VNPAY_URL_URL = "https://cmartshopbe.onrender.com/api/vnpay";
+const VNPAY_URL = "https://cmartshopbe.onrender.com/api/vnpay";
 
 // Function to create VNPay payment
 export const createVNPayPayment = async (amount, orderInfo, returnUrl) => {
